92bc7bd731e1124969c7a05992ffb69cef14b057
Paul <mccarliep@gmail.com>
2017-10-06 23:26:03 -0700

    updated homepage with destinations

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,29 +1,40 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends Application
-{
+class Welcome extends Application {
 
-	function __construct()
-	{
-		parent::__construct();
-	}
+    function __construct() {
+        parent::__construct();
+    }
 
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
+    /**
+     * Homepage for our app
+     */
+    public function index() {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'homepage';
 
-		// build the list of planes, to pass on to our view
-		$source = $this->fleetdata->all();
+        // build the list of planes, to pass on to our view
+        $source = $this->flightsdata->all();
 
-		// pass on the data to present, as the "planes" view parameter
-		$this->data['planes'] = $source;
+        // pass on the data to present, as the "planes" view parameter
+        $this->data['airlines'] = $source;
 
-		$this->render();
-	}
+        $this->render();
+    }
+
+    public function show($key) {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'info';
+
+        // build the list of planes, to pass on to our view
+        $source = $this->flightsdata->get($key);
+
+        // pass on the data to present, adding the author record's fields
+        $this->data = array_merge($this->data, (array) $source);
+
+        $this->render();
+    }
 
 }
