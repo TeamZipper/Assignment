ae9a371b1da459b6cb3b4d093234a4c2b3796447
unhouseledutraquist <andrewlbusto@gmail.com>
2017-11-10 13:08:49 -0800

    closes #27

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('fleetdata','flightsdata');
+$autoload['model'] = array('fleetdata','flightsdata','entity');
--- application/models/Fleetdata.php
+++ application/models/Fleetdata.php
@@ -7,53 +7,21 @@
  *
  * @author jim & andrew
  */
-class Fleetdata extends CI_Model
-{
-	// expressed using long-form array notaiton in case students use PHP 5.x
-	var $data = array(
-		'1' => array("id"           => "avanti",
-                             "manufacturer" => "Piaggo",
-                             "model"        => "Avanti II",
-                             "price"        => "7195",
-                             "seats"        => "8",
-                             "reach"        => "2797",
-                             "cruise"       => "589",
-                             "takeoff"      => "994",
-                             "hourly"       => "977"),
-		'2' => array("id"           => "mustang",
-                             "manufacturer" => "Cessna",
-                             "model"        => "Citation Mustang",
-                             "price"        => "2770",
-                             "seats"        => "4",
-                             "reach"        => "2130",
-                             "cruise"       => "630",
-                             "takeoff"      => "950",
-                             "hourly"       => "1015")
-	);
+class Fleetdata extends CSV_Model {
 
-	// Constructor
-	public function __construct()
-	{
-		parent::__construct();
+    // Constructor
+    public function __construct() {
+        parent::__construct(APPPATH . '../data/fleetdata.csv', 'id');
 
-		// inject each "record" key into the record itself, for ease of presentation
-		foreach ($this->data as $key => $record)
-		{
-			$record['key'] = $key;
-			$this->data[$key] = $record;
-		}
-	}
+        // inject each "record" key into the record itself, for ease of presentation
+        /*foreach ($this->data as $key => $record) {
+            $record['key'] = $key;
+            $this->data[$key] = $record;
+        }*/
+    }
 
-	// retrieve a single quote, null if not found
-	public function get($which)
-	{
-		return !isset($this->data[$which]) ? null : $this->data[$which];
-	}
-
-	// retrieve all of the quotes
-	public function all()
-	{
-		return $this->data;
-	}
-
+    // retrieve a single quote, null if not found
+    public function getPlane($which) {
+        return (array)$this->get($which);
+    }
 }
