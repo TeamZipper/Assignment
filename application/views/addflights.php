64a39c242fd2b5e9cf53ae7f11ff8c060b364dc4
Benjamin <haomingkai@gmail.com>
2017-11-12 21:23:25 -0800

    Flight Page update

---
+++ application/views/addflights.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjamin
+ * Date: 2017-11-12
+ * Time: 9:02 PM
+ */
---
+++ application/views/itemadd.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjamin
+ * Date: 2017-11-12
+ * Time: 5:58 PM
+ */
